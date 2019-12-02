9cbafd70fc12456d33c7460776cd63d1682f4c24
Luis Castillo <castillovaliente@gmail.com>
2019-12-02 03:53:24 -0600

    moving all to react

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
-//index.css
-
-@tailwind base;
-
-@tailwind components;
-
-@tailwind utilities;
+//postcss.config.js
+const tailwindcss = require('tailwindcss');
+module.exports = {
+    plugins: [
+        tailwindcss('./tailwind.js'),
+        require('autoprefixer'),
+    ],
+};
--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import MaterialIcon from '@material/react-material-icon';
+
 
 const SearchBar = ( { handleChange }) => {
   return (
